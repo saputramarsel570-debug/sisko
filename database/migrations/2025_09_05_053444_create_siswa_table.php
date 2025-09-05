f734b3ed416e6fd16d9de5e60682143453293d66
[marsel] <[saputramarsel570@gmail.com]>
2025-09-05 13:17:57 +0700

    5 tabel migration marsel

---
+++ database/migrations/2025_09_05_053444_create_siswa_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nis', 16)->unique();
+            $table->string('nama', 128);
+            $table->ForeignId('kelas_id')->constrained('kelas')->onDelete('cascade');
+            $table->text('alamat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswa');
+    }
+};
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -76,38 +76,16 @@
               <!-- Logo -->
               <div class="app-brand justify-content-center mb-6">
                 <a href="index.html" class="app-brand-link">
-                  <span class="app-brand-logo demo">
-                    <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
-                        fill="#7367F0" />
-                      <path
-                        opacity="0.06"
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
-                        fill="#161616" />
-                      <path
-                        opacity="0.06"
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
-                        fill="#161616" />
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
-                        fill="#7367F0" />
-                    </svg>
-                  </span>
-                  <span class="app-brand-text demo text-heading fw-bold">SISKO</span>
+                    <span class="app-brand-logo demo">
+                        <img src="{{ asset('img/sisko.png') }}" alt="Logo SISKO" width="50" height="40">
+                      </span>
+                      <span class="app-brand-text demo text-heading fw-bold">SISKO</span>
+                      
                 </a>
               </div>
               <!-- /Logo -->
               <h4 class="mb-1">Welcome to SISKO!👋</h4>
-              <p class="mb-6">Please sign-in to your account and start the adventure</p>
+              <p class="mb-6">Masukan Email dan Password untuk Login!</p>
 
               <form id="formAuthentication" class="mb-4" action="index.html" method="GET">
                 <div class="mb-6">
@@ -145,7 +123,7 @@
                   </div>
                 </div>
                 <div class="mb-6">
-                  <button class="btn btn-primary d-grid w-100" type="submit">Login</button>
+                  <button class="btn btn-warning d-grid w-100" type="submit">Login</button>
                 </div>
               </form>
 
